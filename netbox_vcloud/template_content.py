15862a330d8880e8e9bcff098924d3ca169fe2f5
dedys <dedysobr@gmail.com>
2026-06-11 16:52:24 +0300

    fix sync now

--- netbox_vcloud/template_content.py
+++ netbox_vcloud/template_content.py
@@ -4,18 +4,22 @@ from django.utils.html import format_html
 
 
 class CloudSyncConfigButtons(PluginTemplateExtension):
-    model = 'netbox_vcloud.cloudsyncconfig'
+    model = "netbox_vcloud.cloudsyncconfig"
 
     def buttons(self):
-        obj = self.context.get('object')
-        if not obj or obj._meta.label_lower != 'netbox_vcloud.cloudsyncconfig':
+        obj = self.context.get("object")
+        if (
+            not obj
+            or obj._meta.label_lower != "netbox_vcloud.cloudsyncconfig"
+            or not getattr(obj, "pk", None)
+        ):
             return ""
 
-
-        url = reverse('plugins:netbox_vcloud:run_sync_now', args=[obj.pk])
+        url = reverse("plugins:netbox_vcloud:run_sync_now", kwargs={"pk": obj.pk})
         return format_html(
-            f'<a href="{url}" class="btn btn-sm btn-primary">'
-            f'<i class="mdi mdi-sync"></i> Run Sync Now</a>'
+            '<a href="{}" class="btn btn-sm btn-primary">'
+            '<i class="mdi mdi-sync"></i> Run Sync Now</a>',
+            url,
         )
 
 
